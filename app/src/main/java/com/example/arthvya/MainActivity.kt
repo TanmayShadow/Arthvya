f643513992874c6b6db42480a9f31be5610b1d94
TanmayShadow <tanmayshindkar1430@gmail.com>
2024-02-16 19:05:31 +0530

    Input Field is added

--- app/src/main/java/com/example/arthvya/MainActivity.kt
+++ app/src/main/java/com/example/arthvya/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.arthvya
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,11 +10,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.arthvya.ui.theme.ArthvyaTheme
@@ -27,8 +35,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxHeight(),
                     verticalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Welcome()
-                    MyImg()
+//                    Welcome()
+//                    MyImg()
+                    MyInput()
                 }
 
             }
@@ -58,5 +67,20 @@ fun Text()
     ) {
         Welcome()
         MyImg()
+    }
+}
+
+@Composable
+fun MyInput()
+{
+    val context = LocalContext.current
+    var input by remember{
+        mutableStateOf("")
+    }
+//    TextField(value = input, onValueChange = {value->})
+//    TextField(value = input, onValueChange = {input.value=})
+    TextField(value = input, onValueChange = {value->input=value})
+    Button(onClick = { Toast.makeText(context,"$input", Toast.LENGTH_SHORT).show()}) {
+        Text("Click here")
     }
 }
